b6fb4817728e678adce18b8a25165dd8b2c483f5
Franco Cuarterolo <fcuarterolo@gmail.com>
2020-08-23 23:27:55 -0300

    First commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Cuarte Page</h1>
+      <p>lets start coding</p>
+    </div>
+  );
+}
